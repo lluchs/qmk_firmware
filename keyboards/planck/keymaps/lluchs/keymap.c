6e5531c10df0e4158206a4dad7fd73be2f903505
Lukas Werling <lukas.werling@gmail.com>
2017-09-26 12:28:33 +0200

    planck-lluchs: Add Raise+GUI key for i3

--- keyboards/planck/keymaps/lluchs/keymap.c
+++ keyboards/planck/keymaps/lluchs/keymap.c
@@ -32,6 +32,7 @@ enum planck_keycodes {
   RAISE,
   FUNCTION,
   BACKLIT,
+  RAI_GUI, // Raise + LGUI
 };
 
 enum fn_action_idx {
@@ -54,14 +55,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shft/'|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Lead | Alt  | GUI  |Lower |    Space    |Raise | AltGr| GUI  | Hyper| Ctrl |
+ * | Ctrl | ↑-GUI| Alt  | GUI  |Lower |    Space    |Raise | AltGr| GUI  | Hyper| Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
   {KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {F(FUNESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT },
   {KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFTQUOT},
-  {KC_LCTL,   KC_LEAD, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RALT, KC_RGUI, KC_HYPR, KC_RCTL}
+  {KC_LCTL,   RAI_GUI, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RALT, KC_RGUI, KC_HYPR, KC_RCTL}
 },
 
 /* Lower
@@ -169,6 +170,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+    case RAI_GUI:
+      if (record->event.pressed) {
+        register_code(KC_LGUI);
+        layer_on(_RAISE);
+      } else {
+        unregister_code(KC_LGUI);
+        layer_off(_RAISE);
+      }
+      return false;
   }
   return true;
 }
