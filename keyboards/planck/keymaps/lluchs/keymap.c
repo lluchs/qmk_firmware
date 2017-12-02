d91ffb692af9a65a623742030ad93a824cdcb5be
Lukas Werling <lukas.werling@gmail.com>
2017-12-02 18:02:46 +0100

    Bind left space to Shift+Lower

--- keyboards/planck/keymaps/lluchs/keymap.c
+++ keyboards/planck/keymaps/lluchs/keymap.c
@@ -31,6 +31,7 @@ enum planck_layers {
 
 enum planck_keycodes {
   LOWER = SAFE_RANGE,
+  SHLOWER,
   RAISE,
   SWBASE, // switch base layer (normal/gaming)
   FUNCTION,
@@ -65,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {F(FUNESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT },
   {KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFTQUOT},
-  {KC_LCTL,   NUM_GUI, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RALT, SWBASE,  KC_HYPR, KC_RCTL}
+  {KC_LCTL,   NUM_GUI, KC_LGUI, KC_LALT, LOWER,   SHLOWER, KC_SPC,  RAISE,   KC_RALT, SWBASE,  KC_HYPR, KC_RCTL}
 },
 
 /* Qwerty/Gaming
@@ -193,6 +194,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
+    case SHLOWER:
+      if (record->event.pressed) {
+        register_code(KC_LSFT);
+      } else {
+        unregister_code(KC_LSFT);
+      }
+      return process_record_user(LOWER, record);
     case RAISE:
       if (record->event.pressed) {
         layer_on(_RAISE);
